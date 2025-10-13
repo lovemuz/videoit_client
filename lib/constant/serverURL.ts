16077449c2341aa9a5c2aa77fe763019f1e8241d
lovemuz <lovecine98@gmail.com>
2025-10-13 23:36:00 +0900

    11

--- lib/constant/serverURL.ts
+++ lib/constant/serverURL.ts
@@ -4,7 +4,7 @@ import { DEV_MODE } from "@env";
 // const serverURL = 'http://172.30.1.63:5050'
 // const serverURL = 'http://192.168.219.42:5050'
 // const serverURL = 'https://api-dev.nmoment.live'
-const serverURL = 'https://api.nmoment.live'
-
+// const serverURL = 'https://api.nmoment.live'
+const serverURL = 'https://api.videoit.net'
 
 export default serverURL
